e0967f01fb3cd3ff182fcad29da5c9d730bc8bcd
Agnis800 <78017711+Agnis800@users.noreply.github.com>
2023-02-13 08:51:05 +0000

    Pievienots HTML un CSS, iesākts Routing

--- public/index.php
+++ public/index.php
@@ -1,18 +1,22 @@
 <?php
 
-$requestUri = $_SERVER['REQUEST_URI'];
-$templatePath = __DIR__.'/../views/';
+require_once __DIR__.'/../vendor/autoload.php';
 
-switch ($requestUri) {
-    case '/':
-        require $templatePath . 'home.html';
-        break;
-    case '/pica':
-        echo "i equals 1";
-        break;
-    case 2:
-        echo "i equals 2";
-        break;
-    default:
-        echo 'Nothing was found!';
-}
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Route;
+
+
+
+$templatePath = __DIR__.'/../views/';
+$routes = new RouteCollection();
+
+$request = Request::createFromGlobals();
+$requestUri = $request->getRequestUri();
+
+$callback = function() {
+    require $templatePath . 'home.html';
+};
+
+// $routes->add('home', new Route('/'));
+// $routes->add('bye', new Route('/bye'));
