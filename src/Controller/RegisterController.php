424396a322f4b14f0528118f5fc3a2b505b1fe50
Agnis800 <agnisvanags@gmail.com>
2023-03-20 10:51:41 +0200

    Turpinās darbs ar datubāzēm

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -34,7 +34,7 @@ class RegisterController {
             if(empty(trim($_POST["username"]))){
                 $username_err = "Please enter a username.";
             } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))){
-                $username_err = "Username can only containt letters, numbers, and underscores.";
+                $username_err = "Username can only contain letters, numbers, and underscores.";
             } else{
                 $sql = "SELECT UserID FROM user WHERE Username = ?"; //! may not work on sql due to syntax
 
@@ -56,7 +56,8 @@ class RegisterController {
         } else{
                 $first_name = trim($_POST["first_name"]);
         }
-
+        
+        // Validate last name
         if(empty(trim($_POST["last_name"]))){
             $last_name_err = "Please enter a last name.";
         } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["last_name"]))){
@@ -64,13 +65,15 @@ class RegisterController {
         } else{
                 $last_name = trim($_POST["last_name"]);
         }
-
+        
+        // Validate e-mail
         if(empty(trim($_POST["e_mail"]))){
             $e_mail_err = "Please enter a e-mail.";
         } else{
                 $e_mail = trim($_POST["e_mail"]);
         }
-
+        
+        // Validate password
         if(empty(trim($_POST["password"]))){
             $password_err = "Please enter a password.";
         } elseif(strlen(trim($_POST["password"])) < 6){
@@ -78,7 +81,8 @@ class RegisterController {
         } else{
                 $password = trim($_POST["password"]);
         }
-
+        
+        // Check password match
         if(empty(trim($_POST["confirm_password"]))){
             $confirm_password_err = "Please confirm password.";
         } else{
@@ -86,6 +90,27 @@ class RegisterController {
             if(empty($password_err) && ($password != $confirm_password)){
                 $confirm_password_err = "Password did not match.";
             }
+        }
+
+        if(empty($username_err) && empty($first_name_err) && empty($last_name_err) && empty($e_mail_err) && empty($phone_number_err) && empty($password_err) && empty($confirm_password_err)) {
+            $sql = "INSERT INTO user (username, first_name, last_name, e_mail, phone_number, password) VALUES (?, ?)";
+
+            if($stmt = $this->connection->prepare($sql)) {
+                mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+
+                $param_username = $username;
+                $param_password = password_hash($password, PASSWORD_DEFAULT);
+            }
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Redirect to login page
+                header("location: login.php");
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            mysqli_stmt_close($stmt);
         }
 
         }
