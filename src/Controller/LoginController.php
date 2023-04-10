f639b7d368ce5075143ca5567a4a7b36e1d969fa
Agnis800 <agnisvanags@gmail.com>
2023-04-10 18:18:17 +0300

    Fixed syntax issues

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -22,17 +22,17 @@ class LoginController { /* start of logincontroller
 
         session_start();
 
-        if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+        if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){ /* this one doesn't cause the syntax error */
             header("location: home.php");
             exit;
         }
 
-        require_once "config.php";
+        /* require_once "Config.php"; */
 
         $username = $password = "";
         $username_err = $password_err = $login_err = "";
 
-        if($_SERVER["REQUEST_METHOD"] == "POST"){ /* start of validation */
+        if($_SERVER["REQUEST_METHOD"] == "POST"){ /* start of validation (POST) */
 
             if(empty(trim($_POST["username"]))){
                 $username_err = "Please enter username.";
@@ -46,19 +46,17 @@ class LoginController { /* start of logincontroller
                 $password = trim($_POST["password"]);
             }
 
-            if(empty($username_err) && empty($password_err)){
+            if(empty($username_err) && empty($password_err)){ /* start of credental validation */
                 $sql = "SELECT UserID, Username, Password FROM users WHERE Username = :Username";
-            }
 
             $stmt = $this->connection->prepare($sql);
-                $stmt->execute([trim($_POST["username"])]);
+            $stmt->execute([trim($_POST["username"])]);
                 
-                $result = $stmt->fetchColumn();
-        } /* end of validation */
+            $result = $stmt->fetchColumn();
 
-            if($stmt->execute()){ /* start of verification */
+            if($stmt->execute()){ /* start of verification if */ /* !!!start of questenable part!!! */
                 // Check if username exists, if yes then verify password
-                if($stmt->rowCount() == 1){
+                if($stmt->rowCount() == 1){ /* start of rowCount */
                     if($row = $stmt->fetch()){ /* start of login check */
                         $id = $row["id"];
                         $username = $row["id"];
@@ -73,21 +71,23 @@ class LoginController { /* start of logincontroller
                             // Redirect user to home page
                             header("location: home.php");
                         } else{
-                            $login_err = "Invalid username or password."
+                            $login_err = "Invalid username or password.";
                         }
-                      } /* end of password verification */
-                    } /* end of login check */ else{
-                        $login_err = "Invalid username or password"
+                       /* end of password verification */
+                    } /* end of rowCount */ else{
+                        $login_err = "Invalid username or password";
                     }
-                } else{
-                    echo "Oops! Something went wrong. Please try again later."
+                } /* end of verification if */
+                  else{
+                    echo "Oops! Something went wrong. Please try again later.";
                 }
 
                 unset($stmt);
-            } /* end of verification */
+            } /* !!!end of questenable part!!! */
+            } /* end of credental validation */
 
             unset($pdo);
         return render_template($request, 'login.php');
+        } /* end of of validation (POST) */
     } /* end of loginaction */
-
-} /* end of logincontroller
+} /* end of logincontroller */
